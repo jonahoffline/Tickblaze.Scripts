c76ebef1130cc3fcc308d317058b82346fb4d6d2
JonMMR <jon@mythicmarketresearch.com>
2024-11-01 12:51:02 -0400

    Fixed TMS Script Issues With Move to Break Even

--- Tickblaze.Scripts/TradeManagementStrategies/TestJon.cs
+++ Tickblaze.Scripts/TradeManagementStrategies/TestJon.cs
@@ -1,4 +1,4 @@
-using Tickblaze.Scripts.Api.Interfaces.Orders;
+﻿using Tickblaze.Scripts.Api.Interfaces.Orders;
 
 namespace Tickblaze.Scripts.TradeManagementStrategies;
 
@@ -40,6 +40,30 @@ public class TestJon : TradeManagementStrategy
 	[Parameter("Breakeven offset (ticks)", Description = "Enter the number of ticks of profit you want to protect, once the Breakeven Trigger In Ticks has been reached. A positive number will protect that many ticks, a negative number will leave that many ticks at risk, and allow the trade more room to move before closing.")]
 	public int BreakevenOffsetTicks { get; set; } = 1;
 
+	private int DirectionAsInt { get; set; }
+
+	private bool BreakEvenValid => EnableAutoBreakeven && BreakevenTriggerTicks > 0;
+
+	private bool ShouldStopTradeManagementStrategy
+	{
+		get
+		{
+			// Stop immediately if there is no break even.
+			if (!BreakEvenValid)
+			{
+				return true;
+			}
+
+			// If we're waiting for a break even, stop if our position is no longer valid and all of our entry orders were submitted and reached a final state
+			if (Position?.Direction != (DirectionAsInt == 1 ? OrderDirection.Long : OrderDirection.Short) && _orderData.Count > 0 && _orderData.TrueForAll(group => group.Entry.Status != OrderStatus.Pending))
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+
 	private readonly List<OrderData> _orderData = [];
 
 	public TestJon()
@@ -48,50 +72,27 @@ public class TestJon : TradeManagementStrategy
 		Description = @"This TMS script calculates the size of your position based on your Stop Loss Distance (in ticks) and your risk (Calculated in terms of Dollar amount, Percentage Risk or Static Units Risk). It can place 1 or 2 take-profit orders (specified in ticks). You also have the option to set an automatic break-even stop loss function.";
 	}
 
-	protected override void Initialize()
-	{
-		Task.Run(async () =>
-		{
-			while (IsActive)
-			{
-				await Task.Delay(100);
-				if (_orderData.TrueForAll(order => order.Entry.Status is not OrderStatus.Pending) && Position?.Status is not PositionStatus.Open)
-					Stop();
-			}
-		});
-	}
-
 	private void TryMoveToBreakEven()
 	{
-		if (BreakevenTriggerTicks == 0 || Position == null || !EnableAutoBreakeven)
+		if (ShouldStopTradeManagementStrategy)
+		{
+			Stop();
+			return;
+		}
+
+		if (!BreakEvenValid || Position == null)
 		{
 			return;
 		}
 
 		var price = Bars.Close[^1];
-		var ticks = (int)Math.Round((price - Position.EntryPrice) / Symbol.TickSize);
-		var stopPrice = (double?)null;
-
-		if (Position.Direction is OrderDirection.Long)
-		{
-			if (ticks >= BreakevenTriggerTicks)
-			{
-				stopPrice = Position.EntryPrice + BreakevenOffsetTicks * Symbol.TickSize;
-			}
-		}
-		else
-		{
-			if (ticks <= -BreakevenTriggerTicks)
-			{
-				stopPrice = Position.EntryPrice - BreakevenOffsetTicks * Symbol.TickSize;
-			}
-		}
-
-		if (stopPrice == null)
+		var ticksInProfit = (int)Math.Round((price - Position.EntryPrice) / Symbol.TickSize) * DirectionAsInt;
+		if (ticksInProfit < BreakevenTriggerTicks)
 		{
 			return;
 		}
 
+		var stopPrice = Position.EntryPrice + BreakevenOffsetTicks * DirectionAsInt * Symbol.TickSize;
 		foreach (var group in _orderData)
 		{
 			ModifyOrder(group.StopLoss, group.StopLoss.Quantity, stopPrice, null);
@@ -104,7 +105,7 @@ public class TestJon : TradeManagementStrategy
 	{
 		CancelOrder(order);
 
-		var direction = order.Direction is OrderDirection.Long ? 1 : -1;
+		DirectionAsInt = order.Direction is OrderDirection.Long ? 1 : -1;
 		var action = order.Direction is OrderDirection.Long ? OrderAction.Buy : OrderAction.SellShort;
 		var quantity = (double)CalculateQuantity(PositionSizeType, PositionSize, StopLossTicks, RoundingMode.Down);
 		var remainingQuantity = quantity;
@@ -131,11 +132,11 @@ public class TestJon : TradeManagementStrategy
 				}
 			});
 
-			_orderData[^1].ProfitTarget = SetTakeProfit(_orderData[^1].Entry, order.Price + tpTicks * Symbol.TickSize * direction);
-			_orderData[^1].StopLoss = SetStopLoss(_orderData[^1].Entry, order.Price - StopLossTicks * Symbol.TickSize * direction);
+			_orderData[^1].ProfitTarget = SetTakeProfit(_orderData[^1].Entry, order.Price + tpTicks * Symbol.TickSize * DirectionAsInt);
+			_orderData[^1].StopLoss = SetStopLoss(_orderData[^1].Entry, order.Price - StopLossTicks * Symbol.TickSize * DirectionAsInt);
 		}
 
-		if (BreakevenTriggerTicks == 0)
+		if (ShouldStopTradeManagementStrategy)
 		{
 			Stop();
 		}
@@ -157,13 +158,13 @@ public class TestJon : TradeManagementStrategy
 		TryMoveToBreakEven();
 	}
 
+	protected override void OnPositionUpdate()
+	{
+		TryMoveToBreakEven();
+	}
+
 	protected override void OnBarUpdate()
 	{
-		if (Position?.Status is not PositionStatus.Open)
-		{
-			return;
-		}
-
 		TryMoveToBreakEven();
 	}
 
