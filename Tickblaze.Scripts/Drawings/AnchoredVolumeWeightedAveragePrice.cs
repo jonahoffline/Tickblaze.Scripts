d6ee2050de67b31b5f98bf96277dd29bb754f667
Jiri <belochjiri@gmail.com>
2024-11-08 23:18:52 +0100

    Refactor, optimize and cleanup Anchored VWAP (#8)

--- Tickblaze.Scripts/Drawings/AnchoredVolumeWeightedAveragePrice.cs
+++ Tickblaze.Scripts/Drawings/AnchoredVolumeWeightedAveragePrice.cs
@@ -1,256 +1,196 @@
 ﻿namespace Tickblaze.Scripts.Drawings;
 
-public sealed class AnchoredVolumeWeightedAveragePrice : Drawing
+public class AnchoredVolumeWeightedAveragePrice : Drawing
 {
 	[Parameter("VWAP Line Color")]
-	public Color VWAPLineColor
-	{
-		get => _bandSettingsDict[VWAPIds.VWAP].Color;
-		set => _bandSettingsDict[VWAPIds.VWAP].Color = value;
-	}
+	public Color Color { get; set; } = Color.Cyan;
 
 	[Parameter("VWAP Line Thickness"), NumericRange(1, 5)]
-	public int VWAPLineThickness
-	{
-		get => _bandSettingsDict[VWAPIds.VWAP].Thickness;
-		set => _bandSettingsDict[VWAPIds.VWAP].Thickness = value;
-	}
+	public int Thickness { get; set; } = 1;
 
 	[Parameter("VWAP Line Style")]
-	public LineStyle VWAPLineStyle
-	{
-		get => _bandSettingsDict[VWAPIds.VWAP].LineStyle;
-		set => _bandSettingsDict[VWAPIds.VWAP].LineStyle = value;
-	}
+	public LineStyle LineStyle { get; set; } = LineStyle.Solid;
 
-	[Parameter("Band 1 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band1Multiplier
-	{
-		get => _bandSettingsDict[VWAPIds.Band1].Multiplier;
-		set => _bandSettingsDict[VWAPIds.Band1].Multiplier = value;
-	}
+	[Parameter("1. Band Visible?")]
+	public bool BandEnabled1 { get; set; } = false;
 
-	[Parameter("Band 1 Color")]
-	public Color Band1Color
-	{
-		get => _bandSettingsDict[VWAPIds.Band1].Color;
-		set => _bandSettingsDict[VWAPIds.Band1].Color = value;
-	}
+	[Parameter("1. Band deviations"), NumericRange(0, double.MaxValue)]
+	public double BandMultiplier1 { get; set; } = 0.75;
 
-	[Parameter("Band 1 Line Thickness"), NumericRange(0, 5)]
-	public int Band1LineThickness
-	{
-		get => _bandSettingsDict[VWAPIds.Band1].Thickness;
-		set => _bandSettingsDict[VWAPIds.Band1].Thickness = value;
-	}
+	[Parameter("1. Band Color")]
+	public Color BandColor1 { get; set; } = Color.Green;
 
-	[Parameter("Band 1 Line Style")]
-	public LineStyle Band1LineStyle
-	{
-		get => _bandSettingsDict[VWAPIds.Band1].LineStyle;
-		set => _bandSettingsDict[VWAPIds.Band1].LineStyle = value;
-	}
+	[Parameter("1. Band Line Thickness"), NumericRange(1, 5)]
+	public int BandThickness1 { get; set; } = 1;
+
+	[Parameter("1. Band Line Style")]
+	public LineStyle BandLineStyle1 { get; set; } = LineStyle.Solid;
+
+	[Parameter("2. Band Visible?")]
+	public bool BandEnabled2 { get; set; } = false;
 
 	[Parameter("Band 2 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band2Multiplier
-	{
-		get => _bandSettingsDict[VWAPIds.Band2].Multiplier;
-		set => _bandSettingsDict[VWAPIds.Band2].Multiplier = value;
-	}
+	public double BandMultiplier2 { get; set; } = 1.75;
 
 	[Parameter("Band 2 Color")]
-	public Color Band2Color
-	{
-		get => _bandSettingsDict[VWAPIds.Band2].Color;
-		set => _bandSettingsDict[VWAPIds.Band2].Color = value;
-	}
+	public Color BandColor2 { get; set; } = Color.Yellow;
 
-	[Parameter("Band 2 Line Thickness"), NumericRange(0, 5)]
-	public int Band2LineThickness
-	{
-		get => _bandSettingsDict[VWAPIds.Band2].Thickness;
-		set => _bandSettingsDict[VWAPIds.Band2].Thickness = value;
-	}
+	[Parameter("Band 2 Line Thickness"), NumericRange(1, 5)]
+	public int BandThickness2 { get; set; } = 1;
 
 	[Parameter("Band 2 Line Style")]
-	public LineStyle Band2LineStyle
-	{
-		get => _bandSettingsDict[VWAPIds.Band2].LineStyle;
-		set => _bandSettingsDict[VWAPIds.Band2].LineStyle = value;
-	}
+	public LineStyle BandLineStyle2 { get; set; } = LineStyle.Solid;
+
+	[Parameter("3. Band Visible?")]
+	public bool BandEnabled3 { get; set; } = false;
 
 	[Parameter("Band 3 deviations"), NumericRange(0, double.MaxValue)]
-	public double Band3Multiplier
-	{
-		get => _bandSettingsDict[VWAPIds.Band3].Multiplier;
-		set => _bandSettingsDict[VWAPIds.Band3].Multiplier = value;
-	}
+	public double BandMultiplier3 { get; set; } = 2.75;
 
 	[Parameter("Band 3 Color")]
-	public Color Band3Color
-	{
-		get => _bandSettingsDict[VWAPIds.Band3].Color;
-		set => _bandSettingsDict[VWAPIds.Band3].Color = value;
-	}
+	public Color BandColor3 { get; set; } = Color.Red;
 
-	[Parameter("Band 3 Line Thickness"), NumericRange(0, 5)]
-	public int Band3LineThickness
-	{
-		get => _bandSettingsDict[VWAPIds.Band3].Thickness;
-		set => _bandSettingsDict[VWAPIds.Band3].Thickness = value;
-	}
+	[Parameter("Band 3 Line Thickness"), NumericRange(1, 5)]
+	public int BandThickness3 { get; set; } = 1;
 
 	[Parameter("Band 3 Line Style")]
-	public LineStyle Band3LineStyle
-	{
-		get => _bandSettingsDict[VWAPIds.Band3].LineStyle;
-		set => _bandSettingsDict[VWAPIds.Band3].LineStyle = value;
-	}
+	public LineStyle BandLineStyle3 { get; set; } = LineStyle.Solid;
 
+	public IChartPoint Point => Points[0];
 	public override int PointsCount => 1;
 
-	private enum VWAPIds
-	{
-		VWAP,
-		Band1,
-		Band2,
-		Band3
-	}
+	private record LineSettings(double Multiplier, Color Color, int Thickness, LineStyle LineStyle);
 
-	private class BandSettings
-	{
-		public double Multiplier { get; set; }
-		public Color Color { get; set; }
-		public int Thickness { get; set; } = 0;
-		public LineStyle LineStyle { get; set; } = LineStyle.Solid;
-	}
-
-	private readonly Dictionary<VWAPIds, BandSettings> _bandSettingsDict = new()
-	{
-		[VWAPIds.VWAP] = new BandSettings
-		{
-			Color = Color.Cyan,
-			Thickness = 1
-		},
-		[VWAPIds.Band1] = new BandSettings
-		{
-			Multiplier = 0.75,
-			Color = Color.Green
-		},
-		[VWAPIds.Band2] = new BandSettings
-		{
-			Multiplier = 1.75,
-			Color = Color.Yellow
-		},
-		[VWAPIds.Band3] = new BandSettings
-		{
-			Multiplier = 2.75,
-			Color = Color.Red
-		}
-	};
-	private readonly Dictionary<VWAPIds, double> _priorLowerY = [];
-	private readonly Dictionary<VWAPIds, double> _priorUpperY = [];
+	private int? _fromIndex;
+	private Series<double> _cumulativeVolume, _cumulativeTypicalVolume, _cumulativeVariance;
+	private Series<double> _vwap, _deviation;
+	private List<LineSettings> _lineSettings;
 
 	public AnchoredVolumeWeightedAveragePrice()
 	{
 		Name = "Anchored VWAP";
 	}
 
+	protected override void Initialize()
+	{
+		_fromIndex = null;
+		_cumulativeVolume = new();
+		_cumulativeTypicalVolume = new();
+		_cumulativeVariance = new();
+		_vwap = new();
+		_deviation = new();
+
+		_lineSettings = [new(0, Color, Thickness, LineStyle)];
+
+		if (BandEnabled1)
+		{
+			_lineSettings.Add(new(BandMultiplier1, BandColor1, BandThickness1, BandLineStyle1));
+			_lineSettings.Add(new(-BandMultiplier1, BandColor1, BandThickness1, BandLineStyle1));
+		}
+
+		if (BandEnabled2)
+		{
+			_lineSettings.Add(new(BandMultiplier2, BandColor2, BandThickness2, BandLineStyle2));
+			_lineSettings.Add(new(-BandMultiplier2, BandColor2, BandThickness2, BandLineStyle2));
+		}
+
+		if (BandEnabled3)
+		{
+			_lineSettings.Add(new(BandMultiplier3, BandColor3, BandThickness3, BandLineStyle3));
+			_lineSettings.Add(new(-BandMultiplier3, BandColor3, BandThickness3, BandLineStyle3));
+		}
+	}
+
+	public override void SetPoint(IComparable xDataValue, IComparable yDataValue, int index)
+	{
+		var barIndex = Chart.GetBarIndexByXCoordinate(Point.X);
+		var bar = Bars[barIndex];
+
+		Point.Value = (bar.High + bar.Low + bar.Close) / 3;
+	}
+
 	public override void OnRender(IDrawingContext context)
 	{
-		var leftIndex = 0;
-		var rightIndex = 0;
-		var validBarIndexes = CalculateLeftAndRightIndexes(ref leftIndex, ref rightIndex, Points[0].X);
+		var fromIndex = Chart.GetBarIndexByXCoordinate((int)Point.X);
+		var toIndex = Bars.Count - 1;
 
-		if (validBarIndexes == null || validBarIndexes.Length == 0 || leftIndex >= rightIndex - 1 || Points[0].X > Chart.GetXCoordinateByBarIndex(Bars.Count - 1))
+		if (_fromIndex is null || _fromIndex != fromIndex)
+		{
+			_fromIndex = fromIndex;
+
+			for (var index = fromIndex; index < toIndex; index++)
+			{
+				Calculate(index);
+			}
+		}
+
+		Calculate(toIndex);
+
+		var firstVisibleBarIndex = Chart.FirstVisibleBarIndex;
+		if (firstVisibleBarIndex > toIndex)
 		{
 			return;
 		}
 
-		var volumeSum = 0.0;
-		var typicalVolumeSum = 0.0;
-		var varianceSum = 0.0;
-		var vwapPointLeft = new Point(0, 0);
-		var vwapPointRight = new Point(0, 0);
-
-		for (var i = 0; i < validBarIndexes.Length; i++)
+		var lastVisibleBarIndex = Chart.LastVisibleBarIndex;
+		if (lastVisibleBarIndex < fromIndex)
 		{
-			var barIndex = validBarIndexes[i];
-			var bar = Bars[barIndex];
-			var volume = bar.Volume;
-			var typicalPrice = (bar.High + bar.Low + bar.Close) / 3;
-			typicalVolumeSum += volume * typicalPrice;
+			return;
+		}
 
-			if (i == 0)
+		fromIndex = Math.Max(fromIndex, Chart.FirstVisibleBarIndex - 1);
+		toIndex = Math.Min(toIndex, Chart.LastVisibleBarIndex + 1);
+
+		var pointsCount = toIndex - fromIndex + 1;
+		var linePoints = new Dictionary<LineSettings, Point[]>();
+
+		foreach (var key in _lineSettings)
+		{
+			linePoints[key] = new Point[pointsCount];
+		}
+
+		for (var index = fromIndex; index <= toIndex; index++)
+		{
+			var i = index - fromIndex;
+			var x = Chart.GetXCoordinateByBarIndex(index);
+			var vwap = _vwap[index];
+			var deviation = _deviation[index];
+
+			foreach (var (line, points) in linePoints)
 			{
-				volumeSum = bar.Volume;
-				vwapPointRight = new Point(Chart.GetXCoordinateByBarIndex(barIndex), ChartScale.GetYCoordinateByValue(typicalPrice));
-				//update the Y value of the anchor point only if necessary - has performance gains
-				if (Points[0].Y != vwapPointRight.Y)
-				{
-					Points[0].Y = vwapPointRight.Y;
-				}
-
-				//left-edge of all plots start out at the same Y pixel
-				foreach (var id in Enum.GetValues<VWAPIds>())
-				{
-					_priorUpperY[id] = _priorLowerY[id] = vwapPointRight.Y;
-				}
-
-				continue;
+				points[i] = new(x, ChartScale.GetYCoordinateByValue(vwap + deviation * line.Multiplier));
 			}
+		}
 
-			volumeSum += volume;
-			var curVWAP = typicalVolumeSum / volumeSum;
-			var diff = typicalPrice - curVWAP;
-			varianceSum += diff * diff;
-			var deviation = Math.Sqrt(Math.Max(varianceSum / (i + 1), 0));
-
-			//left-edge X pixel is set to the last print X pixel
-			vwapPointLeft.X = vwapPointRight.X;
-			vwapPointRight.X = Chart.GetXCoordinateByBarIndex(barIndex);
-
-			foreach (var id in Enum.GetValues<VWAPIds>())
-			{
-				vwapPointLeft.Y = _priorUpperY[id];
-				vwapPointRight.Y = ChartScale.GetYCoordinateByValue(curVWAP + deviation * _bandSettingsDict[id].Multiplier);
-				_priorUpperY[id] = vwapPointRight.Y;
-				context.DrawLine(vwapPointLeft, vwapPointRight, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
-
-				//Draw the lower line plot only if this is band 1, 2 or 3
-				if (id == VWAPIds.VWAP)
-				{
-					continue;
-				}
-
-				vwapPointLeft.Y = _priorLowerY[id];
-				vwapPointRight.Y = ChartScale.GetYCoordinateByValue(curVWAP - deviation * _bandSettingsDict[id].Multiplier);
-				_priorLowerY[id] = vwapPointRight.Y;
-				context.DrawLine(vwapPointLeft, vwapPointRight, _bandSettingsDict[id].Color, _bandSettingsDict[id].Thickness, _bandSettingsDict[id].LineStyle);
-			}
+		foreach (var (line, points) in linePoints)
+		{
+			context.DrawPolygon(points, null, line.Color, line.Thickness, line.LineStyle);
 		}
 	}
 
-	private int[] CalculateLeftAndRightIndexes(ref int leftIndex, ref int rightIndex, double x1)
+	private void Calculate(int index)
 	{
-		leftIndex = Math.Max(0, Math.Min(Bars.Count - 1, Chart.GetBarIndexByXCoordinate(x1)));
-		rightIndex = Bars.Count - 1;
+		var bar = Bars[index];
+		var volume = bar.Volume;
+		var typicalPrice = (bar.High + bar.Low + bar.Close) / 3;
 
-		//NOTE:  GetBarIndexByXCoordinate() returns -1 if the X coord exceeds the X of the rightmost bar
-		if (rightIndex == -1)
+		if (index == _fromIndex)
 		{
-			rightIndex = Bars.Count - 1;
+			_cumulativeVolume[index] = volume;
+			_cumulativeTypicalVolume[index] = volume * typicalPrice;
+			_vwap[index] = typicalPrice;
+			_cumulativeVariance[index] = 0;
+			_deviation[index] = 0;
+
+			Point.Value = typicalPrice;
 		}
-
-		// Gap bars are null
-		var validBarIndexes = Enumerable.Range(leftIndex, rightIndex - leftIndex)
-			.Where(i => Bars[i] != null)
-			.ToArray();
-
-		var index = leftIndex;
-		leftIndex = validBarIndexes.FirstOrDefault(k => k >= index);
-		index = rightIndex;
-		rightIndex = validBarIndexes.LastOrDefault(k => k <= index);
-		return validBarIndexes;
+		else
+		{
+			_cumulativeVolume[index] = _cumulativeVolume[index - 1] + volume;
+			_cumulativeTypicalVolume[index] = _cumulativeTypicalVolume[index - 1] + volume * typicalPrice;
+			_vwap[index] = _cumulativeTypicalVolume[index] / _cumulativeVolume[index];
+			_cumulativeVariance[index] = _cumulativeVariance[index - 1] + Math.Pow(typicalPrice - _vwap[index], 2);
+			_deviation[index] = Math.Sqrt(_cumulativeVariance[index] / (index + 1 - _fromIndex.Value));
+		}
 	}
 }
