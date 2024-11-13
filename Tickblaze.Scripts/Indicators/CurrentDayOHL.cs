50d9ef5b387684c094d30c778bd4a20963ad00e9
Jon <jon@neurostreet.com>
2024-11-13 16:15:53 -0500

    Updated Current and Prior Day OHL(C) to Use MTF

--- Tickblaze.Scripts/Indicators/CurrentDayOHL.cs
+++ Tickblaze.Scripts/Indicators/CurrentDayOHL.cs
@@ -3,29 +3,22 @@
 /// <summary>
 /// CurrentDayOHL [CDOHL]
 /// </summary>
-public partial class CurrentDayOHL : Indicator
+public class CurrentDayOHL : Indicator
 {
-	[Parameter("Start time"), NumericRange(0, 2359, 1)]
-	public int StartTime { get; set; } = 930;
-
-	[Parameter("End time"), NumericRange(0, 2359, 1)]
-	public int EndTime { get; set; } = 1600;
-
 	[Plot("Open")]
-	public PlotSeries POpen { get; set; } = new(Color.Orange, LineStyle.Dash);
+	public PlotSeries Open { get; set; } = new(Color.Orange, LineStyle.Dash);
 
 	[Plot("High")]
-	public PlotSeries PHigh { get; set; } = new(Color.Red, LineStyle.Dash);
+	public PlotSeries High { get; set; } = new(Color.Red, LineStyle.Dash);
 
 	[Plot("Low")]
-	public PlotSeries PLow { get; set; } = new(Color.Blue, LineStyle.Dash);
+	public PlotSeries Low { get; set; } = new(Color.Blue, LineStyle.Dash);
 
 	private double _highestHigh = double.MinValue;
 	private double _lowestLow = double.MaxValue;
-	private double _open;
-	private string _dayIdStart = "";
-	private string _dayIdEnded = "";
-
+    private BarSeries? _dailyBarSeries;
+    private int _lastDailyBarCount = -1;
+    private double _currentDayOpen;
 	public CurrentDayOHL()
 	{
 		Name = "Current Day OHL";
@@ -33,78 +26,34 @@ public partial class CurrentDayOHL : Indicator
 		IsOverlay = true;
 	}
 
-	protected override void Calculate(int index)
-	{
-		var t0 = ToTime(Bars[index].Time) / 100;
-		var day = Bars[index].Time.Day;
-		var bartimeStr = Bars[index].Time.ToShortDateString();
-		var isEndOfSession = false;
+    protected override void Initialize()
+    {
+        _dailyBarSeries = GetBarSeries(new BarSeriesRequest
+        {
+			Period = new BarPeriod(BarPeriod.SourceType.Day, BarPeriod.PeriodType.Day, 1),
+            Symbol = Bars.Symbol,
+            BarSeriesContract = new BarSeriesContract { Type = BarSeriesContractType.ContinuousBackAdjusted }
+        });
+    }
 
-		//determine if EndTime was encountered today
-		if (t0 >= EndTime && _dayIdEnded.CompareTo(bartimeStr) != 0)
-		{
-			_dayIdEnded = bartimeStr;
-			isEndOfSession = true;
-		}
+    protected override void Calculate(int index)
+    {
+        if (_dailyBarSeries is not { Count: >= 0 })
+            return;
 
-		if (StartTime < EndTime)
-		{
-			//Start time encountered, set variables
-			if (t0 > StartTime && _dayIdStart.CompareTo(bartimeStr) != 0)
-			{
-				_dayIdStart = bartimeStr;
-				_open = Bars[index].Open;
-				_highestHigh = Bars[index].High;
-				_lowestLow = Bars[index].Low;
-			}
-			//while we're between start and end, find HH and LL
-			else if (t0 > StartTime && t0 <= EndTime)
-			{
-				if (_highestHigh < Bars[index].High)
-				{
-					_highestHigh = Math.Max(_highestHigh, Bars[index].High);
-				}
+        if (_lastDailyBarCount != _dailyBarSeries.Count)
+        {
+            _highestHigh = double.MinValue;
+            _lowestLow = double.MaxValue;
+            _currentDayOpen = Bars.Open[index];
+            _lastDailyBarCount = _dailyBarSeries.Count;
+        }
 
-				_lowestLow = Math.Min(_lowestLow, Bars[index].Low);
-			}
-		}
-		else if (StartTime > EndTime)
-		{
-			if (t0 > StartTime && _dayIdStart.CompareTo(bartimeStr) != 0)
-			{
-				_dayIdStart = bartimeStr;
-				_open = Bars[index].Open;
-				_highestHigh = Bars[index].High;
-				_lowestLow = Bars[index].Low;
-			}
-			else if (t0 > StartTime || t0 <= EndTime)
-			{
-				_highestHigh = Math.Max(_highestHigh, Bars[index].High);
-				_lowestLow = Math.Min(_lowestLow, Bars[index].Low);
-			}
-		}
-		else if (StartTime == EndTime)
-		{
-			if (isEndOfSession)
-			{
-				_open = Bars[index].Open;
-				_highestHigh = Bars[index].High;
-				_lowestLow = Bars[index].Low;
-			}
-			else
-			{
-				_highestHigh = Math.Max(_highestHigh, Bars[index].High);
-				_lowestLow = Math.Min(_lowestLow, Bars[index].Low);
-			}
-		}
+        _highestHigh = Math.Max(_highestHigh, Bars.High[index]);
+        _lowestLow = Math.Min(_lowestLow, Bars.Low[index]);
 
-		POpen[index] = _open;
-		PHigh[index] = _highestHigh;
-		PLow[index] = _lowestLow;
-	}
-
-	private static int ToTime(DateTime t)
-	{
-		return t.Hour * 10000 + t.Minute * 100 + t.Second;
+        Open[index] = _currentDayOpen;
+        High[index] = _highestHigh;
+        Low[index] = _lowestLow;
 	}
 }
--- Tickblaze.Scripts/Indicators/PriorDayOHLC.cs
+++ Tickblaze.Scripts/Indicators/PriorDayOHLC.cs
@@ -5,12 +5,6 @@
 /// </summary>
 public partial class PriorDayOHLC : Indicator
 {
-	[Parameter("Start time"), NumericRange(0, 2359, 1)]
-	public int StartTime { get; set; } = 930;
-
-	[Parameter("End time"), NumericRange(0, 2359, 1)]
-	public int EndTime { get; set; } = 1600;
-
 	[Parameter("Show Open")]
 	public bool ShowOpen { get; set; } = true;
 
@@ -34,126 +28,34 @@ public partial class PriorDayOHLC : Indicator
 
 	[Plot("Close")]
 	public PlotSeries Close { get; set; } = new(Color.Yellow, LineStyle.Dash);
+    
+    private BarSeries? _dailyBarSeries;
 
-	private double _hh = double.MinValue;
-	private double _ll = double.MaxValue;
-	private double _o;
-	private double _c;
-	private string _dayIdStart = "";
-	private string _dayIdEnded = "";
-	private Tuple<double, double, double, double> _values = null;
-	public PriorDayOHLC()
+    public PriorDayOHLC()
 	{
 		Name = "Prior Day OHLC";
 		ShortName = "PDOHLC";
 		IsOverlay = true;
 	}
 
-	protected override void Calculate(int index)
+    protected override void Initialize()
+    {
+        _dailyBarSeries = GetBarSeries(new BarSeriesRequest
+        {
+            Period = new BarPeriod(BarPeriod.SourceType.Day, BarPeriod.PeriodType.Day, 1),
+            Symbol = Bars.Symbol,
+            BarSeriesContract = new BarSeriesContract { Type = BarSeriesContractType.ContinuousBackAdjusted }
+        });
+    }
+
+    protected override void Calculate(int index)
 	{
-		var time0 = ToTime(Bars[index].Time) / 100;
-		var barTimeStr = Bars[index].Time.ToShortDateString();
-		var isEndOfSession = false;
-		//determine if EndTime was encountered today
+        if (_dailyBarSeries is not { Count: > 0 })
+            return;
 
-		if (time0 >= EndTime && _dayIdEnded.CompareTo(barTimeStr) != 0)
-		{
-			_dayIdEnded = barTimeStr;
-			isEndOfSession = true;
-			if (_hh != double.MinValue)
-			{
-				_values = new Tuple<double, double, double, double>(_o, _hh, _ll, _c);
-			}
-		}
-
-		if (StartTime < EndTime)
-		{
-			//Start time encountered, set variables
-			if (time0 > StartTime && _dayIdStart.CompareTo(barTimeStr) != 0)
-			{
-				_dayIdStart = barTimeStr;
-				_o = Bars[index].Open;
-				_hh = Bars[index].High;
-				_ll = Bars[index].Low;
-				_c = Bars[index].Close;
-			}
-			//while we're between start and end, find HH and LL
-			else if (time0 > StartTime && time0 <= EndTime)
-			{
-				_hh = Math.Max(_hh, Bars[index].High);
-				_ll = Math.Min(_ll, Bars[index].Low);
-				_c = Bars[index].Close;
-			}
-		}
-		else if (StartTime > EndTime)
-		{
-			//Start time encountered today, set variables
-			if (time0 > StartTime && _dayIdStart.CompareTo(barTimeStr) != 0)
-			{
-				_dayIdStart = barTimeStr;
-				_o = Bars[index].Open;
-				_hh = Bars[index].High;
-				_ll = Bars[index].Low;
-				_c = Bars[index].Close;
-			}
-			//while we're between start and end, find HH and LL
-			else if (time0 > StartTime || time0 <= EndTime)
-			{
-				_hh = Math.Max(_hh, Bars[index].High);
-				_ll = Math.Min(_ll, Bars[index].Low);
-				_c = Bars[index].Close;
-			}
-		}
-		else if (StartTime == EndTime)
-		{
-			//end and start are at the same time, so reset variables at that time
-			if (isEndOfSession)
-			{
-				_o = Bars[index].Open;
-				_hh = Bars[index].High;
-				_ll = Bars[index].Low;
-				_c = Bars[index].Close;
-			}
-			//continue to find HH and LL
-			else
-			{
-				_hh = Math.Max(_hh, Bars[index].High);
-				_ll = Math.Min(_ll, Bars[index].Low);
-				_c = Bars[index].Close;
-			}
-		}
-
-		//set plots to prior day ohlc
-		if (_values != null)
-		{
-			Open[index] = 0;
-			High[index] = 0;
-			Low[index] = 0;
-			Close[index] = 0;
-			if (ShowOpen)
-			{
-				Open[index] = _values.Item1;
-			}
-
-			if (ShowHigh)
-			{
-				High[index] = _values.Item2;
-			}
-
-			if (ShowLow)
-			{
-				Low[index] = _values.Item3;
-			}
-
-			if (ShowClose)
-			{
-				Close[index] = _values.Item4;
-			}
-		}
-	}
-
-	private static int ToTime(DateTime t)
-	{
-		return t.Hour * 10000 + t.Minute * 100 + t.Second;
+        Open[index] = _dailyBarSeries[^1].Open;
+        High[index] = _dailyBarSeries[^1].High;
+        Low[index] = _dailyBarSeries[^1].Low;
+        Close[index] = _dailyBarSeries[^1].Close;
 	}
 }
