d648714513746a747af3b9b3b46c03f5ced08831
whis <whis@users.noreply.github.com>
2024-11-21 22:17:46 +0000

    deploy: d3214cbbd8f02ce25e2901c4cfbf448562505f04

---
+++ public/main.js
@@ -0,0 +1,15 @@
+export default {
+    defaultTheme: 'auto',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/Tickblaze/Tickblaze.Scripts',
+        title: 'GitHub'
+      },
+      {
+        icon: 'twitter',
+        href: 'https://x.com/tickblaze',
+        title: 'Twitter'
+      }
+    ]
+  }
