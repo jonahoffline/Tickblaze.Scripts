d5a2cbb00507a2f4c1f1d32a364647cbbc9933cf
Jonathan <jon@neurostreet.com>
2025-01-09 03:49:10 -0500

    Safe Cleanup

--- src/Indicators/VolumeProfileComposite.cs
+++ src/Indicators/VolumeProfileComposite.cs
@@ -22,104 +22,86 @@ public partial class VolumeProfileComposite : Indicator, VolumeProfile.ISettings
 	public int RowsSize { get; set; } = 24;
 
 	[NumericRange(0, 100)]
-	[Parameter("Histo Width %", Description = "Width of the histogram as a percentage of box width",
-		GroupName = StyleGroupName)]
+	[Parameter("Histo Width %", Description = "Width of the histogram as a percentage of box width", GroupName = StyleGroupName)]
 	public double RowsWidthPercent { get; set; } = 30;
 
-	[Parameter("Histo Location", Description = "Location of histogram (left or right side of box)",
-		GroupName = StyleGroupName)]
+	[Parameter("Histo Location", Description = "Location of histogram (left or right side of box)", GroupName = StyleGroupName)]
 	public VolumeProfile.PlacementType RowsPlacement { get; set; } = VolumeProfile.PlacementType.Left;
 
 	[NumericRange(0, 100)]
-	[Parameter("Value Area %", Description = "Percentage of total volume considered in the value area",
-		GroupName = StyleGroupName)]
+	[Parameter("Value Area %", Description = "Percentage of total volume considered in the value area", GroupName = StyleGroupName)]
 	public double ValueAreaPercent { get; set; } = 70;
 
 	[Parameter("Value Area Color", Description = "Color of the value area", GroupName = StyleGroupName)]
 	public Color ValueAreaColor { get; set; } = "#bf808080";
 
-	[Parameter("Above Value Area Color", Description = "Color of area above the value area",
-		GroupName = StyleGroupName)]
+	[Parameter("Above Value Area Color", Description = "Color of area above the value area", GroupName = StyleGroupName)]
 	public Color ValueAreaAboveColor { get; set; } = "#80ff0000";
 
-	[Parameter("Below Value Area Color", Description = "Color of area below the value area",
-		GroupName = StyleGroupName)]
+	[Parameter("Below Value Area Color", Description = "Color of area below the value area", GroupName = StyleGroupName)]
 	public Color ValueAreaBelowColor { get; set; } = "#80ff0000";
 
 	[Parameter("Outline Color", Description = "Color of the volume profile outline box", GroupName = StyleGroupName)]
 	public Color BoxLineColor { get; set; } = "#80ffffff";
 
-	[Parameter("Outline Thickness", Description = "Thickness of the volume profile outline box",
-		GroupName = StyleGroupName)]
+	[Parameter("Outline Thickness", Description = "Thickness of the volume profile outline box", GroupName = StyleGroupName)]
 	public int BoxLineThickness { get; set; } = 1;
 
-	[Parameter("Outline Style", Description = "Style of the volume profile outline box (solid, dashed, etc.)",
-		GroupName = StyleGroupName)]
+	[Parameter("Outline Style", Description = "Style of the volume profile outline box (solid, dashed, etc.)", GroupName = StyleGroupName)]
 	public LineStyle BoxLineStyle { get; set; } = LineStyle.Dot;
 
-	[Parameter("VAH Line Visible?", Description = "Show/Hide the Value Area High (VAH) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VAH Line Visible?", Description = "Show/Hide the Value Area High (VAH) line", GroupName = StyleGroupName)]
 	public bool VahLineVisible { get; set; } = false;
 
 	[Parameter("VAH Line Color", Description = "Color of the Value Area High (VAH) line", GroupName = StyleGroupName)]
 	public Color VahLineColor { get; set; } = Color.White;
 
-	[Parameter("VAH Line Thickness", Description = "Thickness of the Value Area High (VAH) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VAH Line Thickness", Description = "Thickness of the Value Area High (VAH) line", GroupName = StyleGroupName)]
 	public int VahLineThickness { get; set; } = 2;
 
 	[Parameter("VAH Line Style", Description = "Style of the Value Area High (VAH) line", GroupName = StyleGroupName)]
 	public LineStyle VahLineStyle { get; set; } = LineStyle.Solid;
 
-	[Parameter("VAL Line Visible?", Description = "Show/Hide the Value Area Low (VAL) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VAL Line Visible?", Description = "Show/Hide the Value Area Low (VAL) line", GroupName = StyleGroupName)]
 	public bool ValLineVisible { get; set; } = false;
 
 	[Parameter("VAL Line Color", Description = "Color of the Value Area Low (VAL) line", GroupName = StyleGroupName)]
 	public Color ValLineColor { get; set; } = Color.White;
 
-	[Parameter("VAL Line Thickness", Description = "Thickness of the Value Area Low (VAL) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VAL Line Thickness", Description = "Thickness of the Value Area Low (VAL) line", GroupName = StyleGroupName)]
 	public int ValLineThickness { get; set; } = 2;
 
 	[Parameter("VAL Line Style", Description = "Style of the Value Area Low (VAL) line", GroupName = StyleGroupName)]
 	public LineStyle ValLineStyle { get; set; } = LineStyle.Solid;
 
-	[Parameter("POC Line Visible?", Description = "Show/Hide the Point of Control (POC) line",
-		GroupName = StyleGroupName)]
+	[Parameter("POC Line Visible?", Description = "Show/Hide the Point of Control (POC) line", GroupName = StyleGroupName)]
 	public bool PocLineVisible { get; set; } = true;
 
 	[Parameter("POC Line Color", Description = "Color of the Point of Control (POC) line", GroupName = StyleGroupName)]
 	public Color PocLineColor { get; set; } = Color.Yellow;
 
-	[Parameter("POC Line Thickness", Description = "Thickness of the Point of Control (POC) line",
-		GroupName = StyleGroupName)]
+	[Parameter("POC Line Thickness", Description = "Thickness of the Point of Control (POC) line", GroupName = StyleGroupName)]
 	public int PocLineThickness { get; set; } = 2;
 
 	[Parameter("POC Line Style", Description = "Style of the Point of Control (POC) line", GroupName = StyleGroupName)]
 	public LineStyle PocLineStyle { get; set; } = LineStyle.Solid;
 
-	[Parameter("Show VAH/VAL/POC Prices?", Description = "Displays prices for VAH, VAL, and POC levels",
-		GroupName = StyleGroupName)]
+	[Parameter("Show VAH/VAL/POC Prices?", Description = "Displays prices for VAH, VAL, and POC levels", GroupName = StyleGroupName)]
 	public bool ShowPrices { get; set; } = false;
 
 	[Parameter("Font", Description = "Font for displaying VAH/VAL/POC prices", GroupName = StyleGroupName)]
 	public Font Font { get; set; } = new("Arial", 12);
 
-	[Parameter("VWAP Enabled?", Description = "Show/Hide the Volume Weighted Average Price (VWAP) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VWAP Enabled?", Description = "Show/Hide the Volume Weighted Average Price (VWAP) line", GroupName = StyleGroupName)]
 	public bool VwapEnabled { get; set; } = false;
 
-	[Parameter("VWAP Line Color", Description = "Color of the Volume Weighted Average Price (VWAP) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VWAP Line Color", Description = "Color of the Volume Weighted Average Price (VWAP) line", GroupName = StyleGroupName)]
 	public Color VwapLineColor { get; set; } = Color.Blue;
 
-	[Parameter("VWAP Line Thickness", Description = "Thickness of the Volume Weighted Average Price (VWAP) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VWAP Line Thickness", Description = "Thickness of the Volume Weighted Average Price (VWAP) line", GroupName = StyleGroupName)]
 	public int VwapLineThickness { get; set; } = 1;
 
-	[Parameter("VWAP Line Style", Description = "Style of the Volume Weighted Average Price (VWAP) line",
-		GroupName = StyleGroupName)]
+	[Parameter("VWAP Line Style", Description = "Style of the Volume Weighted Average Price (VWAP) line", GroupName = StyleGroupName)]
 	public LineStyle VwapLineStyle { get; set; } = LineStyle.Solid;
 
 	public enum CompositionType
@@ -133,7 +115,7 @@ public partial class VolumeProfileComposite : Indicator, VolumeProfile.ISettings
 	private BarSeries _bars;
 	private IExchangeSession _lastSession;
 	private VolumeProfile.Area<VolumeProfileComposite> _currentArea;
-	private List<VolumeProfile.Area<VolumeProfileComposite>> _areas = [];
+	private readonly List<VolumeProfile.Area<VolumeProfileComposite>> _areas = [];
 
 	public VolumeProfileComposite()
 	{
